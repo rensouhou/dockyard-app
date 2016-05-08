a8275bad51f81cf8a4cd60bb78f44e6613893e19
Stefan Rimaila <stefan@fusetools.com>
2016-05-08 21:06:37 +0300

    Gitignore update, start tests anew

---
+++ test/reducers/player.spec.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import player from '../../app/reducers/player';
+
+describe('reducers', () => {
+  describe('player', () => {
+    it('should handle the initial state', () => {
+      expect(true).to.equal(true);
+    });
+  });
+});
