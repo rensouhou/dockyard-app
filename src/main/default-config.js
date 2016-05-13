3b34d501ddc6ccee19344659768db0f004facf4c
Stefan Rimaila <stefan@fusetools.com>
2016-05-13 12:15:00 +0200

    Base configuration seed

--- src/main/default-config.js
+++ src/main/default-config.js
@@ -1,3 +1,16 @@
+/**
+ * @overview
+ *  Base configuration used for seeding on initial launch
+ *
+ * @since 0.1.0
+ * @author Stefan Rimaila <stefan@rimaila.fi>
+ */
 export default {
-  remoteDebugPort: 8642
+  core: {
+    remoteDebugPort: 8642,
+    ppapiFlashPathOSX: '%APP_ROOT%/lib/PepperFlashPlayer.plugin',
+    ppapiFlashVersion: '21.0.0.197'
+  },
+  game: {},
+  player: {}
 };
