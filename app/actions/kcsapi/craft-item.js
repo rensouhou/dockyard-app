507056246ef5ec68de9227f396de47d8bcdccade
Stefan Rimaila <stefan@fusetools.com>
2016-06-09 23:51:10 +0200

    `CRAFT_ITEM` handler function is a bit prettier and works better.

--- app/actions/kcsapi/craft-item.js
+++ app/actions/kcsapi/craft-item.js
@@ -4,25 +4,27 @@
  *
  * @since 0.1.0
  */
-import R from 'ramda';
-import { Map } from 'immutable';
+import { fromJS, List } from 'immutable';
 import { asNumber, asBool } from '../../transformers/primitive';
 import { parseMaterialsRecipe, parseMaterialArray, asRecord } from '../../transformers/api/materials';
 
+const getSlotItem = (slotItem = {}) => ({
+  id: slotItem.api_id,
+  slotItemId: slotItem.api_slotitem_id
+});
+
 export default function CRAFT_ITEM({ body, postBody }) {
-  return Map({
+  return fromJS({
     flags: {
       successful: asBool(body.api_create_flag),
       usedDevelopmentMaterials: asBool(body.api_shizai_flag)
     },
     consumed: {
-      recipe: asRecord(parseMaterialsRecipe(R.map(asNumber, postBody)))
+      materials: asRecord(parseMaterialsRecipe(List(postBody).map(asNumber)))
     },
-    materials: asRecord(parseMaterialArray(body.api_material)),
-    // @todo(@stuf): replace with Maybe monad (`sanctuary`)
-    slotItem: !R.is(Object, body.api_slot_item) ? null : {
-      id: body.api_slot_item.api_id,
-      slotItemId: body.api_slot_item.api_slotitem_id
-    }
+    player: {
+      materials: asRecord(parseMaterialArray(body.api_material))
+    },
+    slotItem: getSlotItem(body.api_slot_item)
   });
 }
