82dca5444dd75a4e0d77ece65a5bfd3b81da9765
Stefan Rimaila <stefan@fusetools.com>
2016-06-10 23:31:13 +0300

    Remove duplicate from codeclimate.yml

--- app/actions/kcsapi/get-quest-list.js
+++ app/actions/kcsapi/get-quest-list.js
@@ -5,18 +5,25 @@
  * @since 0.1.0
  */
 import { Map, fromJS } from 'immutable';
+import { Enum } from '../../helpers';
 import { asNumber } from '../../transformers/primitive';
 import { quest as transformQuest } from '../../transformers/api/quest';
 
-const questListTabs = {
+const questListTabs = Enum({
   DEFAULT: 0,
-  DAILY: 1
-};
+  DAILY: 1,
+  WEEKLY: 2,
+  MONTHLY: 3,
+  ONE_TIME: 4,
+  OTHERS: 5,
+  IN_PROGRESS: 9
+});
 
-export default function GET_QUEST_LIST({ body }) {
+export default function GET_QUEST_LIST({ body, postBody }) {
   return fromJS({
     questCount: asNumber(body.api_count),
     currentPage: asNumber(body.api_disp_page),
+    currentlyActiveTab: questListTabs(postBody.api_tab_page),
     totalPageCount: asNumber(body.api_page_count),
     quests: fromJS(body.api_list).toMap()
                                  .map((q) => fromJS(transformQuest(q)))
