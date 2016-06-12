de93aad06964c6176303d09a5b8dcde1d9c0c9a1
Stefan Rimaila <stefan@fusetools.com>
2016-06-12 22:33:02 +0300

    Fixed.

--- app/containers/ConfigurePage.js
+++ app/containers/ConfigurePage.js
@@ -1,10 +1,4 @@
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Configure from '../components/Configure';
-import { saveConfiguration, readConfiguration } from '../actions/core';
 
-const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators({ saveConfiguration, readConfiguration }, dispatch)
-});
-
-export default connect(null, mapDispatchToProps)(Configure);
+export default connect()(Configure);
