fc8a1ea95118fd95b9ee2111bf38c3bd03144c34
Stefan Rimaila <stefan@fusetools.com>
2016-06-22 19:51:52 +0300

    Added asRecord method

--- app/transformers/api/player-fleet.js
+++ app/transformers/api/player-fleet.js
@@ -5,11 +5,13 @@
  */
 import { fromJS } from 'immutable';
 import { notEmpty, getArrayOrDefault } from '../primitive';
+import { FleetRecord } from '../../records';
 import { mission } from './mission';
 
 /**
+ * @type {function}
  * @param {Object} d
- * @returns {FleetRecord|Map}
+ * @returns {IMap<string, *>}
  */
 export const playerFleet = (d) => fromJS({
   flagship: d.api_flagship,
@@ -19,3 +21,10 @@ export const playerFleet = (d) => fromJS({
   name: d.api_name,
   ships: getArrayOrDefault(d.api_ship).filter(notEmpty)
 });
+
+/**
+ * @type {function}
+ * @param {Object} d
+ * @returns {FleetRecord}
+ */
+export const asFleetRecord = (d) => new FleetRecord(playerFleet(d));
