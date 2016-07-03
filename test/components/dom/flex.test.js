40b50268ab5a6bdb5080135aba1b4f9de4664a2d
Stefan Rimaila <stefan@fusetools.com>
2016-07-03 15:16:17 +0200

    Added dummy flex test

---
+++ test/components/dom/flex.test.js
@@ -0,0 +1,28 @@
+/* eslint no-unused-expressions: 0 */
+/**
+ * @overview
+ * @since 0.4.0
+ */
+import { expect } from 'chai';
+import { spy } from 'sinon';
+import React from 'react';
+import {
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag,
+  findRenderedDOMComponentWithClass,
+  Simulate
+} from 'react-addons-test-utils';
+import { Flex } from '../../app/components/dom';
+
+function setup() {
+  const actions = {};
+  const attributes = {};
+  const component = renderIntoDocument(<Flex {...attributes} />);
+  return { component };
+}
+
+describe('DOM Flex component', () => {
+  it('should be true', () => {
+    expect(true).to.be(true);
+  });
+});
