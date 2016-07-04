a05489b7b3b3c2dfe5e43f50365d3fec276d5fd6
Stefan Rimaila <stefan@fusetools.com>
2016-07-04 22:59:07 +0200

    Basic helpers

--- app/core/game/data-helpers.js
+++ app/core/game/data-helpers.js
@@ -1,4 +1,35 @@
 /**
  * @overview
+ *  Provides basic helper functions for common operations with records,
+ *  like {@link isAbyssalShip} or {@link getShipSpeed}.
  *
+ * @since 0.4.0
  */
+import { cond, always, equals, T } from 'ramda';
+
+/**
+ * @type {function}
+ * @param {ShipRecord} ship
+ * @returns {boolean}
+ */
+export const isAbyssalShip = (ship) => ship.shipId >= 500 && ship.shipId <= 900;
+
+/**
+ * @type {function}
+ * @param {ShipRecord} ship
+ * @returns {number}
+ * @todo Use less magic numbers
+ */
+export const getShipSpeed = (ship) => cond([
+  [equals(0), always('zero')],
+  [equals(5), always('five')],
+  [equals(10), always('ten')],
+  [T, always('fallthrough')]
+])(ship.stats.speed);
+
+/**
+ * @type {function}
+ * @param {ShipRecord} ship
+ * @returns {number}
+ */
+export const getShipRange = (ship) => ship.stats.range;
