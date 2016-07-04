3fea7f907dd37e57ce37c99d1ee59fb5d9133dd2
Stefan Rimaila <stefan@fusetools.com>
2016-07-04 23:12:17 +0200

    Added pending tests

---
+++ test/core/data-helpers.spec.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+describe('core/data-helpers', () => {
+  describe('isAbyssalShip', () => {
+    // @todo pending
+    it('should correctly identify an abyssal ship');
+  });
+  describe('getShipSpeed', () => {
+    // @todo pending
+    it('should get the correct speed for an FBB');
+  });
+  describe('getShipRange', () => {
+    it('should get the correct range enum');
+  });
+});
